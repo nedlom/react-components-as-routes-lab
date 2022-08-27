121cf837945bcca02c140acbb51fe41608548ac0
Nick <nelom3@gmail.com>
2022-08-27 14:04:31 -0400

    commit tree 83e4859041053f5d2e75594ab73ba5039e3129d7

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,15 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(a => {
+        return (
+          <div>{a.name}
+          <ul>
+            {a.movies.map(m => <li>{m}</li>)}
+            </ul></div>
+        )
+      })}
     </div>
   );
 };
